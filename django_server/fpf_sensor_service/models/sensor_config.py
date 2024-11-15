c1f99ddd030f2fc81c3b7c6ac26c699e168e3ac3
julian <j.schoepe@ostfalia.de>
2024-11-15 13:08:35 +0100

    Added Additional Information field to Sensor config and created Migrations

--- django_server/fpf_sensor_service/models/sensor_config.py
+++ django_server/fpf_sensor_service/models/sensor_config.py
@@ -11,8 +11,9 @@ class SensorConnectionTypes(ListableEnum):
 
 class SensorConfig(models.Model):
     """
-    SensorConfig model with the id as a GUID and the intervall as an Integer
+    SensorConfig model with the id as a GUID and the interval as an Integer
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    intervalSeconds = models.IntegerField(required=True)
-    sensorConnectionType = models.CharField(required=True, choices=SensorConnectionTypes.list())
+    intervalSeconds = models.IntegerField(blank=False)
+    sensorConnectionType = models.CharField(max_length=256, blank=False)
+    additionalInformation = models.CharField(blank=True, max_length=1024)
