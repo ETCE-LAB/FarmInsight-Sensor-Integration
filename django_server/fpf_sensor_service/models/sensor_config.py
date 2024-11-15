525a4c30b386af8d0eef7f7b43dd8c5c05f7878f
julian <j.schoepe@ostfalia.de>
2024-11-15 12:50:02 +0100

    added sensorConnectionType to SensorConfig model

--- django_server/fpf_sensor_service/models/sensor_config.py
+++ django_server/fpf_sensor_service/models/sensor_config.py
@@ -1,5 +1,12 @@
 import uuid
 from django.db import models
+from fpf_sensor_service.utils import ListableEnum
+
+
+class SensorConnectionTypes(ListableEnum):
+    PinConnection = 'PinConnection'
+    PicoConnection = 'PicoConnection'
+    FarmbotApiConnection = 'FarmbotApiConnection'
 
 
 class SensorConfig(models.Model):
@@ -7,4 +14,5 @@ class SensorConfig(models.Model):
     SensorConfig model with the id as a GUID and the intervall as an Integer
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    intervalSeconds = models.IntegerField()
+    intervalSeconds = models.IntegerField(required=True)
+    sensorConnectionType = models.CharField(required=True, choices=SensorConnectionTypes.list())
