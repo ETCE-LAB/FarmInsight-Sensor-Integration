b53460e26d436ce4f37a190965f334c77d73af28
Julian Schöpe <j.schoepe@ostfalia.de>
2024-11-29 14:17:56 +0100

    removed trailing slashes on urls

--- django_server/fpf_sensor_service/urls.py
+++ django_server/fpf_sensor_service/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('sensors/types', get_available_sensor_types, name='get_available_sensor_types'),
     path('sensors', post_sensor, name='post_sensor'),
     path('sensors/<str:sensor_id>', SensorView.as_view(), name='sensor_views'),
-    path('fpf-ids/', post_fpf_id, name='post_fpf_id'),
-    path('api-keys/', post_api_key, name='post_api_key'),
+    path('fpf-ids', post_fpf_id, name='post_fpf_id'),
+    path('api-keys', post_api_key, name='post_api_key'),
 ]
